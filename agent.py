f55a6812b1a0ab427a982fb9ee286563ddbf4698
Spencer R Loggia <spencer.loggia98@gmail.com>
2020-07-05 23:29:35 -0400

    Super-human performance

--- agent.py
+++ agent.py
@@ -8,25 +8,27 @@ import human_interface
 import time
 import sys
 
+
 class Agent:
     def __init__(self, lr=.0001):
         self.interface = None
-        self.model = ConvNetwork().float()#.cuda(0)
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.model = ConvNetwork().float().cuda(0)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.cur_frame = None
         self.short_term_memory = list()
         self.final_reward = int()
 
     def train(self, NUM_GAMES, max_drop=.5, max_explore=.4, disp_iter=10, save_iter=500):
-        self.short_term_memory = list()
-        self.final_reward = int()
         if max_drop > 1 or max_explore > 1:
             print('Invalid Hyperparameter. Learning Aborted', sys.stderr)
             return
         data_file = open('./data/scores.csv', 'w')
         data_file.write("iter,score\n")
         self.model = self.model.float()
-        for i in range(NUM_GAMES): 
+        for i in range(NUM_GAMES):
+            torch.cuda.empty_cache()
+            self.short_term_memory = []
+            self.final_reward = 0
             if (i+1) % save_iter == 0:
                 file = open('./models/trained' + str(i+1) + '.pkl', 'wb')
                 torch.save(self.model, file)
@@ -44,41 +46,46 @@ class Agent:
 
     # initialize recursion
     def meditate(self, gamma):
-        '''
-        Preform batch graient decent on this play
-        :param prev:
-        :param completion_ratio:
-        :param max_drop:
-        :param max_explore:
-        :param count:
-        :return:
-        '''
+        DEATH_COST = -100
+        STAY_ALIVE_REWARD = 1
         num_states = len(self.short_term_memory)
         shape = self.short_term_memory[0][0].shape
-        batch_x = torch.zeros(num_states, shape[0], shape[1])#.cuda(0)
-        batch_y = torch.zeros(num_states)#.cuda(0)
-        move_indices = torch.zeros(num_states).long()
+        batch_x = torch.zeros(num_states, shape[0], shape[1]).cuda(0)
+        batch_y = torch.zeros(num_states).cuda(0)
+        move_indices = torch.zeros(num_states).long().cuda(0)
 
-        for i in range(num_states):
+        final_move = num_states-1
+        batch_y[final_move] = DEATH_COST # death penilized with -100
+        batch_x[final_move] = self.short_term_memory[final_move][0]
+        move_indices[final_move] = self.short_term_memory[final_move][1].cuda(0)
+        for i in range(num_states - 2, -1, -1):
             batch_x[i] = self.short_term_memory[i][0]
-            move_indices[i] = self.short_term_memory[i][1]
-            batch_y[i] = (num_states - i) * np.power(gamma, i)
+            move_indices[i] = self.short_term_memory[i][1].cuda(0)
+            batch_y[i] = STAY_ALIVE_REWARD + (gamma * batch_y[i+1])
+
         # TODO: add gradients, clean, up, training logit
         batch_y_hat = self.model(batch_x, batch_size=num_states, training=False)
-
         self.optimizer.zero_grad()
         move_indices = move_indices.reshape([num_states, 1])
         batch_y_hat = batch_y_hat.gather(1, move_indices).reshape([-1]) #select moves that were made
         loss = nn.functional.mse_loss(batch_y_hat, batch_y)
-        loss.backward()
-        self.optimizer.step()
+        try:
+            loss.backward()
+            self.optimizer.step()
+        except RuntimeError:
+            pass
 
-    def play_game(self, max_explore, completion_ratio, count=0, save_exp=True):
+    def play_game(self, max_explore, completion_ratio, save_exp=True):
         game_over = False
         state = 0
+        count = 0
         while not game_over:
-            x = torch.from_numpy(self.cur_frame)#.cuda(0)
+            time.sleep(.1)
+            x = torch.from_numpy(self.cur_frame).cuda(0)
             exp_reward = self.model(x.float(), training=False)
+
+            if count % 10 == 0:
+                print(exp_reward)
 
             epsilon = np.random.random(1)
             if epsilon <= (1 - max_explore) * np.exp(.7 * completion_ratio):
@@ -102,6 +109,7 @@ class Agent:
                 self.cur_frame = self.cur_frame[4:46, :]
             if state > 0:
                 game_over = True
+            count += 1
 
         self.final_reward = state
         print("scored: " + str(state))
@@ -120,9 +128,9 @@ if __name__ == "__main__":
     #torch.cuda.set_device(0)
     torch.autograd.set_detect_anomaly(True)
     bob = Agent(lr=.001)
-   # bob.model = torch.load('./models/trained1000.pkl')
-   # bob.eval(10)
-    bob.train(NUM_GAMES=30, max_drop=.3, max_explore=.6, disp_iter=10, save_iter=10)
+    bob.model = torch.load('./models/trained50000.pkl')
+    #bob.eval(10)
+    bob.train(NUM_GAMES=10, max_drop=0, max_explore=0, disp_iter=1, save_iter=5000)
     #torch.cuda.device_count()
 
 
